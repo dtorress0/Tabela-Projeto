82ec7a9bd69256b4f442b9162f4b93b612686d4e
Davi Oliveira Torres <132406997+dtorress0@users.noreply.github.com>
2024-06-03 21:11:30 -0300

    first commit

---
+++ ProjetoTabela/js/buscar encomenda.js
@@ -0,0 +1,53 @@
+var botaoBuscar = document.querySelector("#buscar-encomendas");
+
+botaoBuscar.addEventListener("click", function(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:3000/encomendas");
+    xhr.addEventListener("load", function(){
+        var resposta = xhr.responseText;
+        var encomendas = JSON.parse(resposta);
+
+        encomendas.forEach(function(cada_encomenda){
+            adicionarEncomendaNaTabela(cada_encomenda);
+        });
+    });
+    xhr.send();
+});
+
+function adicionarEncomendaNaTabela(encomenda) {
+    var tabela = document.getElementById('tabela-precos').getElementsByTagName('tbody')[0];
+    var novaLinha = document.createElement('tr');
+
+    var celulaNome = document.createElement('td');
+    celulaNome.textContent = encomenda.nome;
+    novaLinha.appendChild(celulaNome);
+
+    var celulaServico = document.createElement('td');
+    celulaServico.textContent = encomenda.produto;
+    novaLinha.appendChild(celulaServico);
+
+    var celulaQuantidade = document.createElement('td');
+    celulaQuantidade.textContent = encomenda.qtde;
+    novaLinha.appendChild(celulaQuantidade);
+
+    var celulaValor = document.createElement('td');
+    celulaValor.textContent = formatarParaMoeda(encomenda.valor_unitario);
+    novaLinha.appendChild(celulaValor);
+
+    var celulaTotal = document.createElement('td');
+    celulaTotal.textContent = formatarParaMoeda(calculaTotal(parseFloat(encomenda.qtde), parseFloat(encomenda.valor_unitario)));
+    novaLinha.appendChild(celulaTotal);
+
+    tabela.appendChild(novaLinha);
+
+    celulaNome.classList.add("nome");
+    novaLinha.classList.add("cliente");
+}
+
+function formatarParaMoeda(valor) {
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+function calculaTotal(quantidade, valorUnitario) {
+    return quantidade * valorUnitario;
+}
